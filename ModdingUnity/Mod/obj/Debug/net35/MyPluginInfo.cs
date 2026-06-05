63ec69f9a1b9de57f8df0686ba70dd7c66a78734
Pyran99 <130940584+Pyran99@users.noreply.github.com>
2026-06-04 22:20:30 -0600

    added git ignore

--- ModdingUnity/Mod/obj/Debug/net35/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Mod
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Mod";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
